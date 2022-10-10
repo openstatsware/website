f47a7e36f04b5624cb53f98d31048417edb84e7c
Ya Wang <68403409+ywang-gilead@users.noreply.github.com>
2022-10-10 10:51:33 -0700

    Initiating the website (#1)
    * initialization with website skeleton
    
    * set up the website
    
    * Update what_we_do.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update what_we_do.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update what_we_do.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update what_we_do.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update what_we_do.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update mmrm_R_package.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update join_us.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update join_us.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update join_us.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update join_us.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update news.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update mmrm_R_package.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * rename files
    
    * add helper function to read member list
    
    * Update mmrm_R_package.qmd
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update data/members.csv
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * Update data/members.csv
    
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    
    * update members.csv
    
    * update homepage
    
    * Update data/members.csv
    
    Co-authored-by: Brian Lang <BrianLang@users.noreply.github.com>
    
    * Update goals.qmd
    
    Co-authored-by: Brian Lang <BrianLang@users.noreply.github.com>
    
    * update homepage
    
    * embed google form in the contact us tab
    
    * incorporate comments from WG
    
    * update news
    
    Co-authored-by: Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>
    Co-authored-by: Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
    Co-authored-by: Brian Lang <BrianLang@users.noreply.github.com>

---
+++ utils/read_members.R
@@ -0,0 +1,31 @@
+read.members <- function(subset)
+{
+  ## load packages
+  packs.html <- c("knitr", "pander", "dplyr")
+  for (i in 1:length(packs.html)){library(packs.html[i], character.only = TRUE)}
+
+  knitr::opts_chunk$set(echo = TRUE)
+  panderOptions('table.alignment.default', function(df){ifelse(sapply(df, is.numeric), 'right', 'left')})
+
+  ## read member list
+  members <- as.data.frame(read.csv(paste(path, "/data/members.csv", sep = "")))
+
+  ## get subset of member list
+  subset <- as.name(subset)
+  members.subset <- (members %>%
+                       filter(!!subset == 1) %>%
+                       select(Firstname, Lastname, Affiliation) %>%
+                       arrange(Firstname, Lastname) %>%
+                       rename(`First Name` = Firstname, `Last Name` = Lastname))
+
+  ## output member list
+  members.out <- kable(members.subset)
+  cat(knit(text = members.out, quiet = TRUE))
+}
+
+
+
+
+
+
+
