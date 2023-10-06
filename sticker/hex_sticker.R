627382d8ab26ef26d7f56772ca99e3e9ef89de71
Alessandro Gasparini <alessandro.gasparini@reddooranalytics.se>
2023-10-06 16:56:20 +0200

    New hex & hex pipeline

--- sticker/hex_sticker.R
+++ sticker/hex_sticker.R
@@ -1,22 +1,24 @@
-library(hexSticker)
+library(cropcircles)
+library(magick)
+library(tidyverse)
+library(ggpath)
+library(ragg)
 library(glue)
 
-for (.dpi in c(300, 600, 1200)) {
-  print(
-    sticker(
-      subplot = "sticker/software.png",
-      s_x = 1,
-      s_y = 0.92,
-      s_width = 0.8,
-      s_height = 0.8,
-      filename = glue("sticker/sticker-{.dpi}.png"),
-      dpi = .dpi,
-      asp = 0.863,
-      h_color = "#455A64",
-      h_fill = "#FFFEFE",
-      package = ""
-    )
-  )
+x <- image_read("sticker/hex.png")
+xhex <- hex_crop(x, border_size = 150, border_colour = "#FFC300")
+hex <- ggplot() +
+  geom_from_path(aes(0.5, 0.5, path = xhex)) +
+  xlim(0, 1) +
+  ylim(0, 1) +
+  theme_void() +
+  coord_fixed()
+hex
+
+dpi <- c(300, 600, 1200)
+for (this in dpi) {
+  ggsave(filename = glue("sticker/openstatsware-hex-{this}.png"), plot = hex, height = 5, width = 5, dpi = this, device = agg_png)
 }
+
 # Attribution for png icon:
 # <a href="https://www.flaticon.com/free-icons/technology" title="technology icons">Technology icons created by Kalashnyk - Flaticon</a>
