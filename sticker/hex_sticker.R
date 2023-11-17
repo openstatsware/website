7a029fe4ea8614a0bdeb3de25312c356504a563d
Alessandro Gasparini <alessandro@ellessenne.xyz>
2023-11-17 21:03:29 +0100

    Rebranding (#74)
    Co-authored-by: Daniel Sabanes Bove <daniel.sabanes_bove@roche.com>

--- sticker/hex_sticker.R
+++ sticker/hex_sticker.R
@@ -1,22 +1,52 @@
-library(hexSticker)
+library(cropcircles)
+library(magick)
+library(tidyverse)
+library(ggpath)
+library(ragg)
 library(glue)
+library(svglite)
 
-for (.dpi in c(300, 600, 1200)) {
-  print(
-    sticker(
-      subplot = "sticker/software.png",
-      s_x = 1,
-      s_y = 0.92,
-      s_width = 0.8,
-      s_height = 0.8,
-      filename = glue("sticker/sticker-{.dpi}.png"),
-      dpi = .dpi,
-      asp = 0.863,
-      h_color = "#455A64",
-      h_fill = "#FFFEFE",
-      package = ""
-    )
-  )
+x <- image_read("sticker/hex.png")
+xhex <- hex_crop(x, border_size = 150, border_colour = "#FFC300")
+hex <- ggplot() +
+  geom_from_path(aes(0.5, 0.5, path = xhex)) +
+  xlim(0, 1) +
+  ylim(0, 1) +
+  theme_void() +
+  coord_fixed()
+hex
+
+# .png versions
+dpi <- c(300, 600, 1200)
+for (this in dpi) {
+  ggsave(filename = glue("sticker/openstatsware-hex-{this}.png"), plot = hex, height = 5, width = 5, dpi = this, device = agg_png)
 }
+
+# .svg version
+ggsave(filename = "sticker/openstatsware-hex.svg", plot = hex, height = 5, width = 5, dpi = this, device = svglite::svglite)
+
 # Attribution for png icon:
 # <a href="https://www.flaticon.com/free-icons/technology" title="technology icons">Technology icons created by Kalashnyk - Flaticon</a>
+
+# Rounded text version
+xr <- image_read("sticker/hex-round.png")
+xrhex <- hex_crop(xr, border_size = 150, border_colour = "#FFC300")
+hexr <- ggplot() +
+  geom_from_path(aes(0.5, 0.5, path = xrhex)) +
+  xlim(0, 1) +
+  ylim(0, 1) +
+  theme_void() +
+  coord_fixed()
+hexr
+
+# .png versions
+dpi <- c(300, 600, 1200)
+for (this in dpi) {
+  ggsave(filename = glue("sticker/openstatsware-hex-round-{this}.png"), plot = hexr, height = 5, width = 5, dpi = this, device = agg_png)
+}
+
+# .svg version
+ggsave(filename = "sticker/openstatsware-hex-round.svg", plot = hexr, height = 5, width = 5, dpi = this, device = svglite::svglite)
+
+# Attribution for developer.png icon:
+# <a href="https://www.flaticon.com/free-icons/web-development" title="web-development icons">Web-development icons created by Freepik - Flaticon</a>
