22eea46fc44854d87aaeb7c7b243d78e75de27f9
Daniel Sabanes Bove <danielinteractive@users.noreply.github.com>
2026-04-04 22:02:29 +0800

    172: add alumni section in People (#173)
    * add alumni section in People
    
    * address copilot review comments

--- utils/read_members.R
+++ utils/read_members.R
@@ -1,31 +1,28 @@
-read.members <- function(subset)
-{
+read.members <- function(subset) {
   ## load packages
   packs.html <- c("knitr", "pander", "dplyr")
-  for (i in 1:length(packs.html)){library(packs.html[i], character.only = TRUE)}
+  for (i in seq_along(packs.html)) {
+    library(packs.html[i], character.only = TRUE)
+  }
 
   knitr::opts_chunk$set(echo = TRUE)
-  panderOptions('table.alignment.default', function(df){ifelse(sapply(df, is.numeric), 'right', 'left')})
+  panderOptions('table.alignment.default', function(df) {
+    ifelse(sapply(df, is.numeric), 'right', 'left')
+  })
 
   ## read member list
   members <- as.data.frame(read.csv(paste(path, "/data/members.csv", sep = "")))
 
   ## get subset of member list
-  subset <- as.name(subset)
+  subset_symbol <- as.name(subset)
   members.subset <- (members %>%
-                       filter(!!subset == 1) %>%
-                       select(Firstname, Lastname, Affiliation) %>%
-                       arrange(Firstname, Lastname) %>%
-                       rename(`First Name` = Firstname, `Last Name` = Lastname))
+    filter(!!subset_symbol == 1) %>%
+    select(Firstname, Lastname, Affiliation) %>%
+    arrange(Firstname, Lastname) %>%
+    rename(`First Name` = Firstname, `Last Name` = Lastname))
+  message(paste("Number of ", subset, ": ", nrow(members.subset), sep = ""))
 
   ## output member list
   members.out <- kable(members.subset)
   cat(knit(text = members.out, quiet = TRUE))
 }
-
-
-
-
-
-
-
